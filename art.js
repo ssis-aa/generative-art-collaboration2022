fd8ef29c4d68ecc5ef6938dbb0dbc7f0f8e8405d
DanDC25 <111406215+DanDC25@users.noreply.github.com>
2022-12-05 11:53:43 +0700

    Update art.js

--- art.js
+++ art.js
@@ -10,7 +10,8 @@ function setup() {
   
   
   // Dan: adding transparency to the right part
-  
+  let TRANSPARENCY = 4
+  var COLORS = [[216, 164, 127, TRANSPARENCY],[239, 131, 84, TRANSPARENCY],[238, 75, 106, TRANSPARENCY],[223, 59, 87, TRANSPARENCY],[15, 113, 115, TRANSPARENCY], [179, 255, 179, TRANSPARENCY], [202, 231, 185, TRANSPARENCY], [205, 247, 246, TRANSPARENCY], [16, 255, 203, TRANSPARENCY], [181, 248, 254, TRANSPARENCY], [151, 249, 249, TRANSPARENCY], [164, 222, 249, TRANSPARENCY]]
   
   
   // Khang: using noise for the left part of the image
