93a058bb6f7e03905a95cccca2182a0e276ec97e
Matthias Kreier <98946372+matthiaskreier@users.noreply.github.com>
2022-12-05 11:50:03 +0700

    Create art.js

---
+++ art.js
@@ -0,0 +1,28 @@
+// Generative art 2022
+
+function setup() {
+  createCanvas(1000, 500);
+  background(220);
+
+  // William: translation of geometric shapes in the right
+  //          part of the image
+  
+  
+  
+  // Dan: adding transparency to the right part
+  
+  
+  
+  // Khang: using noise for the left part of the image
+  
+  
+  
+  // Dongjae: image manipulation of the whole created image
+  
+  
+  
+}
+
+function draw() {
+
+}
