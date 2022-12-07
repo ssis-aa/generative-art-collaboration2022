8a20ccc0f521f50a368fdeee6be065d76552af91
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-12-07 15:23:41 +0700

    tidy up code

--- art.js
+++ art.js
@@ -1,30 +1,28 @@
 // Generative art 2022
 
 // functions created for this project
-
 // William
 function cubesection() {
-stroke(51);
-         fill (213,90,79)
+  stroke(51);
+  fill(213, 90, 79);
   square(750, -75, 50);
-              fill (0)
+  fill(0);
   square(770, -145, 50);
-           fill (219,224,103)
+  fill(219, 224, 103);
   square(760, -125, 50);
-            fill (0)
+  fill(0);
   square(710, -125, 50);
-         fill (255)
+  fill(255);
   square(712.5, -123.5, 45);
   strokeWeight(4);
-     fill (0,0,0,1)
+  fill(0, 0, 0, 1);
   square(760, -200, 50);
   strokeWeight(1);
   noStroke();
 }
 
-// Dan's Transparency 
+// Dan's Transparency
 let TRANSPARENCY = 1;
-
 
 function setup() {
   createCanvas(1000, 500);
@@ -43,8 +41,8 @@ function setup() {
   );
   square(random(30, 160) + width / 2, random(20, 140), random(40, 150));
 
-  // William: translation of geometric shapes in the right
-  //          part of the image
+  // William: 
+  // translation of geometric shapes in the right part of the image
   let NUM_SECTIONS = 10;
 
   for (var i = 0; i < NUM_SECTIONS; i++) {
@@ -57,64 +55,57 @@ function setup() {
   }
 
   // Dan: adding transparency to the right part
-  
+
   var COLORS = [
     [216, 164, 127, TRANSPARENCY],
-    [239, 131, 84, TRANSPARENCY],
-    [238, 75, 106, TRANSPARENCY],
-    [223, 59, 87, TRANSPARENCY],
-    [15, 113, 115, TRANSPARENCY],
+    [239, 131, 84,  TRANSPARENCY],
+    [238, 75,  106, TRANSPARENCY],
+    [223, 59,  87,  TRANSPARENCY],
+    [15,  113, 115, TRANSPARENCY],
     [179, 255, 179, TRANSPARENCY],
     [202, 231, 185, TRANSPARENCY],
     [205, 247, 246, TRANSPARENCY],
-    [16, 255, 203, TRANSPARENCY],
+    [16,  255, 203, TRANSPARENCY],
     [181, 248, 254, TRANSPARENCY],
     [151, 249, 249, TRANSPARENCY],
     [164, 222, 249, TRANSPARENCY],
   ];
-  
-  let random_X = random(1000, 500)
-  let random_Y = random(1000, 500)
-  
-  function Cube(){
+
+  let random_X = random(1000, 500);
+  let random_Y = random(1000, 500);
+
+  function Cube() {
     fill(COLORS);
     square(random_X, random_Y, 50);
   }
-  for (var k = 0; k < 20; k++){
-    Cube()
+  for (var k = 0; k < 20; k++) {
+    Cube();
   }
 
   // Khang: using noise for the left part of the image
   // correct location for Khang's code: random numbers
-  let x_coordinate = 500;         
-  for (var plot_x = 0; plot_x < 500; plot_x++) {   
+  let x_coordinate = 500;
+  for (var plot_x = 0; plot_x < 500; plot_x++) {
     beginShape();
     x_noise = noise(x_coordinate) * 500;
     strokeWeight(20);
     point(plot_x, x_noise);
     x_coordinate++;
     console.log(plot_x);
-  }  
-  
+  }
+
   // Dongjae: image manipulation of the whole created image
 
   // Dan II: manipulate the whole image a second time
-  
+
   // Image manipulation 2022-12-07
   // Dan
-  
-  
+
   // Dongjae
-  
-  
-  
+
   // Khang
-  
-  
+
   // William
-  
-  
-  
 }
 
 function draw() {
